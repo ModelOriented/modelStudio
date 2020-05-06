18367a393227abc5da30816a8dee9b20f9573a82
hbaniecki <hbaniecki@gmail.com>
2020-05-07 00:07:25 +0200

    fix tests

--- tests/testthat/test_warnings_and_errors.R
+++ tests/testthat/test_warnings_and_errors.R
@@ -6,7 +6,7 @@ testthat::test_that("new_observation as list", {
   testthat::expect_warning(
     modelStudio::modelStudio(explain_glm,
                              new_observation = as.list(titanic_test[1,-9]),
-                             show_info = v)
+                             show_info = v, B = 3)
   )
 })
 
@@ -14,7 +14,7 @@ testthat::test_that("check_single_prediction error", {
   testthat::expect_error(
     modelStudio::modelStudio(explainer_xgb,
                              new_observation = model_matrix_train[1,],
-                             show_info = v)
+                             show_info = v, B = 3)
   )
 })
 
@@ -25,15 +25,15 @@ testthat::test_that("deprecated modelStudioOptions", {
 })
 
 ms <- modelStudio::modelStudio(explain_rf, apartments[1:2,], N = 5, B = 2, show_info = v)
-new_ms <- modelStudio::ms_update_options(ms, explain_rf, B = 2, show_info = v,
-                                         new_observation = apartments[1,],
-                                         new_observation_y = apartments$m2.price[1])
+new_ms <- modelStudio::ms_update_observations(ms, explain_rf, B = 2, show_info = v,
+                                              new_observation = apartments[1,],
+                                              new_observation_y = apartments$m2.price[1])
 
 testthat::test_that("duplicated ids", {
   testthat::expect_is(new_ms, "modelStudio")
   testthat::expect_warning(
-    modelStudio::ms_update_options(ms, explain_rf, B = 2, show_info = v,
-                                   new_observation = apartments[1,],
-                                   new_observation_y = apartments$m2.price[1])
+    modelStudio::ms_update_observations(ms, explain_rf, B = 2, show_info = v,
+                                       new_observation = apartments[1,],
+                                       new_observation_y = apartments$m2.price[1])
   )
 })
