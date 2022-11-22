be15973c84ce0b7cd756fa3c6d42489808fdbced
dominik-aigora <dominik.rafacz@aigora.com>
2022-11-22 10:53:06 +0100

    Change default plot for modelStudio from FI to BD

--- inst/d3js/modelStudio.js
+++ inst/d3js/modelStudio.js
@@ -505,7 +505,7 @@ function initializeStudio() {
 
   if (facetData.length >= 1) {
     svg.selectAll('.enterChoiceButton').filter('#enterChoiceButton0').dispatch('click');
-    svg.select("#chosePlotButton0").select("#FI").dispatch('click');
+    svg.select("#chosePlotButton0").select("#BD").dispatch('click');
   }
 
   if (facetData.length >= 2) {
